17a166b07752d7a8063bc326f94a1c298adbf3e7
Abdug'affor Abdurahimov <abdugaffor.abdurakhimov@gmail.com>
2023-06-13 16:55:52 +0500

    Particles.tsx implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Providers from "@/app/Providers"
+import Particles from "@/components/Particles"
 import { About, Footer, Navbar } from "@/sections"
 
 export default function Home() {
@@ -9,6 +10,10 @@ export default function Home() {
         <Navbar />
         <About />
         <Footer />
+        <Particles
+          className="absolute inset-0 -z-10 animate-fade-in"
+          quantity={1000}
+        />
       </main>
     </Providers>
   )
