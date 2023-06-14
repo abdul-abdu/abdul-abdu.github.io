3c3716900fada3205df6f8d10679cab54e261971
Abdug'affor Abdurahimov <abdugaffor.abdurakhimov@gmail.com>
2023-06-14 19:41:27 +0500

    Update

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -26,32 +26,34 @@ const ITEMS = [
 
 export default function Navbar() {
   return (
-    <nav className="w-full py-4 font-mono text-lg">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-          <div className="flex w-full justify-between align-baseline">
-            <div className="flex">
-              {ITEMS.map((item) => (
-                <ScrollLink
-                  key={item.id}
-                  className="flex justify-center p-2"
-                  href={item.href}
-                >
-                  {item.title}
-                </ScrollLink>
-              ))}
-            </div>
-            <ThemeToggler />
+    <>
+      <nav className="w-full py-4 font-mono text-lg">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex w-full justify-between align-baseline">
+              <div className="flex">
+                {ITEMS.map((item) => (
+                  <ScrollLink
+                    key={item.id}
+                    className="flex justify-center p-2"
+                    href={item.href}
+                  >
+                    {item.title}
+                  </ScrollLink>
+                ))}
+              </div>
 
-            <ArrowUpButton
-              className="fixed bottom-0 right-0 m-3"
-              onClick={() => {
-                window.scrollTo({ behavior: "smooth", top: 0 })
-              }}
-            />
+              <ThemeToggler />
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <ArrowUpButton
+        className="fixed bottom-0 right-0 m-3"
+        onClick={() => {
+          window.scrollTo({ behavior: "smooth", top: 0 })
+        }}
+      />
+    </>
   )
 }
