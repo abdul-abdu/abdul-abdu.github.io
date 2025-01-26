2a01b4b6ebd450bbb1feb3cd681df1ac642589c1
Abdul <abdugaffar.abdurahimov@danads.se>
2025-01-26 14:09:32 +0500

    WIP

--- robots.ts
+++ robots.ts
@@ -1,6 +1,5 @@
 import type { MetadataRoute } from 'next'
 
-export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
   const isNoindex = process.env.APP_INDEX_MODE === 'NOINDEX'
