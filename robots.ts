c730bfdb4977601b125b583345a7650ad4cbe43a
Abdul <abdugaffar.abdurahimov@danads.se>
2025-01-26 13:58:37 +0500

    WIP

---
+++ robots.ts
@@ -0,0 +1,21 @@
+import type { MetadataRoute } from 'next'
+
+export const dynamic = 'force-static'
+
+export default function robots(): MetadataRoute.Robots {
+  const isNoindex = process.env.APP_INDEX_MODE === 'NOINDEX'
+  const domain = process.env.NEXT_PUBLIC_APP_DOMAIN
+
+  return {
+    rules: isNoindex
+      ? {
+          userAgent: '*',
+          disallow: '/',
+        }
+      : {
+          userAgent: '*',
+          allow: '/',
+        },
+    sitemap: `${domain}/sitemap.xml`,
+  }
+}
