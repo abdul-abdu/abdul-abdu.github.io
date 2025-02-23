ceb60aaa559b484daa8a830c645b8d468c915e92
Abdul <abdugaffar.abdurahimov@danads.se>
2025-02-23 18:16:56 +0500

    Tailwind upgrade

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -6,10 +6,10 @@ export function KBarSearchButton() {
         name="search"
         placeholder="Search..."
         type="text"
-        className="block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm/6"
+        className="block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-xs ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm/6"
       />
       <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
-        <kbd className="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-400">
+        <kbd className="inline-flex items-center rounded-sm border border-gray-200 px-1 font-sans text-xs text-gray-400">
           ⌘K
         </kbd>
       </div>
